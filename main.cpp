38dd2ac911213c5d161883d29f0a3825b107eea0
Dairell <dairellhannan@yahoo.com>
2025-12-25 13:17:33 +0800

    csv mode (not complete)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 
 using namespace std;
+struct Attendance
+{
+    int studentID;
+    string name;
+    int status;
+};
+
+
+
+
+void displayCSV(Attendance sheet[], int rowCount)
+{
+    if (rowCount == 0)
+        {
+            cout << "-------------------------" << endl;
+            cout << "Attendance sheet is empty" << endl;
+            cout << "-------------------------" << endl;
+        }
+        return;
+
+
+
+    cout << endl;
+    cout << "---------------------" << endl;
+    cout << "StudentID,Name,Status" << endl;
+    cout << "---------------------" << endl;
+
+    for (int i = 0; i < rowCount; i++)
+        {
+            cout << sheet[i].studentID << ","
+                 << sheet[i].name << ","
+                 << sheet[i].status << endl;
+        }
+}
 
 int main()
 {
